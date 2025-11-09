805fb39a8324eda02d5ddf333fe18dc9e3ea0d66
Davidson Mochahari <davidsonmochahari06@gmail.com>
2025-11-09 20:46:44 +0530

    removed fetch function

--- app/homepage/services-banner.tsx
+++ app/homepage/services-banner.tsx
@@ -5,8 +5,6 @@ import CardLight from "./components/card-light"
 
 export default async function ServicesBanner() {
 
-    const data: any = await fetchFromStrapi("services")
-
     return (
         <div className="flex flex-col bg-light justify-between items-center gap-20 items-center width-full xl:mt-30 mt-10 px-10 xl:px-20 py-15">
             <div className="flex flex-col gap-3 items-center w-full">
