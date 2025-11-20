d371647601b420fc6b7ab504aef666392981f0b8
Davidson Mochahari <davidsonmochahari06@gmail.com>
2025-11-20 20:33:24 +0530

    Fixed error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,15 @@
-"use client"
 import Footer from "../components/footer"
 import Header from "../components/header"
 import ScrollToTopButton from "../components/scroll-to-top"
 import VideoBanner from "./components/videobanner"
 
 export default function About() {
-    return(
+    return (
         <div className="bg-light">
             <Header />
-            <VideoBanner/>
-            <Footer/>
-            <ScrollToTopButton/>
+            <VideoBanner />
+            <Footer />
+            <ScrollToTopButton />
         </div>
     )
 }
