3cf9ab7f08a1108690f878d011b57028e3e68cf3
Davidson Mochahari <davidsonmochahari06@gmail.com>
2025-11-20 19:21:58 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 <meta name="color-scheme" content="light only" />
@@ -31,6 +33,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
+        <Analytics />
         {children}
       </body>
     </html>
